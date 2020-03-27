2eb444151ba08c5c2592cf4123aaaaed20c0139d
Jelena <35238061+algebrabender@users.noreply.github.com>
2020-03-27 15:44:21 +0100

    Update Main.cpp

--- CPolynom/Main.cpp
+++ CPolynom/Main.cpp
@@ -1,28 +1,43 @@
 #include "CPolynom.h"
+#include <iostream>
+#include <exception>
+
+using namespace std;
 
 int main()
 {
-	CPolynom* p1 = new CPolynom(2);
-	CPolynom* p2 = new CPolynom(3);
-	
-	p1->Coef(0, 10.02);
-	p1->Coef(2, 19.94);
-	p2->Coef(0, 1.08);
-	p2->Coef(1, 20.14);
-	p2->Coef(3, 15.03);
-	
-	p1->print();
-	p2->print();
+	try
+	{
+		CPolynom* p1 = new CPolynom(2);
+		CPolynom* p2 = new CPolynom(3);
+		CPolynom* p3 = new CPolynom(0);
 
-	CPolynom* p3 = new CPolynom(0);
-	p3->Add(p1, p2);
-	p3->print();
+		p1->Coef(0, 10.02);
+		p1->Coef(2, 19.94);
+		p2->Coef(0, 1.08);
+		p2->Coef(1, 20.14);
+		p2->Coef(3, 15.03);
 
-	p3 = p3->Mul(p1, p2);
-	p3->print();
+		p1->print();
+		p2->print();
 
-	delete p1;
-	delete p2;
-	delete p3;
+		p3->Add(p1, p2);
+		p3->print();
+
+		p3 = p3->Mul(p1, p2);
+		p3->print();
+
+		delete p1;
+		delete p2;
+		delete p3;
+	}
+	catch (exception exc)
+	{
+		cout << exc.what() << endl;
+	}
+	catch (...)
+	{
+		cout << "Nepoznata greska" << endl;
+	}
 	return 0;
 }
