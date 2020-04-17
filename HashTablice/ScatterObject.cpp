1e5c20260c21cf692dfead293c6c6dc0f9796872
Jelena <35238061+algebrabender@users.noreply.github.com>
2020-04-17 15:04:35 +0200

    Update ScatterObject.cpp

--- HashTablice/ScatterObject.cpp
+++ HashTablice/ScatterObject.cpp
@@ -1,19 +1,19 @@
 #include "ScatterObject.h"
 
 ScatterObject::ScatterObject()
+	:HashObject()
 {
-	HashObject();
 	status = 0;
 }
 
 ScatterObject::ScatterObject(int key)
+	:HashObject(key)
 {
-	HashObject(key);
 	status = 0;
 }
 
 ScatterObject::ScatterObject(int key, char* record)
+	: HashObject(key, record)
 {
-	HashObject(key, record);
 	status = 0;
 }
