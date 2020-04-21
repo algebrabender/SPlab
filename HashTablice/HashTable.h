2e512e8e7d51e021fd619603b9f95ab88ba459d8
Jelena <35238061+algebrabender@users.noreply.github.com>
2020-04-21 16:07:34 +0200

    Update HashTable.h

--- HashTablice/HashTable.h
+++ HashTablice/HashTable.h
@@ -6,11 +6,9 @@ class HashTable {
 protected:
 	unsigned int length; //velicina tablice
 	unsigned int count; //broj elemenata u tablici
-	
-	unsigned int h1(HashObject* object); //hash funkcija, metod deljenja
-	unsigned int h2(HashObject* object); //hash f-ja, metod sredina kvadrata
-	unsigned int h3(HashObject* object); //hash f-ja, Fibonacijev metod
-	unsigned int f(int iKey); //int->unsigned
+
+	unsigned int h(HashObject* object); //hash f-ja
+	unsigned int f(int iKey); //int->unsigned, metod sredina kvadrata
 	unsigned int f(double dKey); //double->unsigned
 	unsigned int f(char* sKey); //string->unsigned
 public:
