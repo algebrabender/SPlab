a585e7f47aa12d6305597669f19d5ec2d9517830
Jelena <35238061+algebrabender@users.noreply.github.com>
2020-11-08 13:06:22 +0100

    Update Edge.h

--- Grafovi/Edge.h
+++ Grafovi/Edge.h
@@ -7,7 +7,7 @@ class Edge {
 public:
 	LinkedNode* dest; //odredisni cvor
 	Edge* link; //sl u listi
-	//int weight; //idk sta za tezinum ovamo je W drugacije od T za info
+	//int weight;
 	Edge() : dest(nullptr), link(nullptr) {};
 	Edge(LinkedNode* dest, Edge* link) :dest(dest), link(link) {};
 };
